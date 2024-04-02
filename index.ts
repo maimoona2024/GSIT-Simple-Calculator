7473dab5f34bc904406d22744402f717b1e76a08
Maimoona <160620135+maimoona2024@users.noreply.github.com>
2024-04-02 09:16:31 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,31 @@
+import inquirer from "inquirer";
+
+async function performOperations() {
+    let answers = await inquirer.prompt([
+        {message: "Enter First Number", type: "number", name: "firstNumber"},
+        {message: "Enter Second Number", type: "number", name: "secondNumber"},
+        {
+            message:"Select one number to perform operations",
+            type: "list",
+            name: "operator", // Change "Operator" to "operator"
+            choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+        }
+    ]);
+
+    console.log(answers);
+
+    // Condition statement
+    if (answers.operator === "Addition"){
+        console.log(answers.firstNumber + answers.secondNumber);
+    } else if (answers.operator === "Subtraction"){
+        console.log(answers.firstNumber - answers.secondNumber);
+    } else if (answers.operator === "Multiplication"){
+        console.log(answers.firstNumber * answers.secondNumber);
+    } else if (answers.operator === "Division"){
+        console.log(answers.firstNumber / answers.secondNumber);
+    } else {
+        console.log("Please select valid operator");
+    }
+}
+
+performOperations();
